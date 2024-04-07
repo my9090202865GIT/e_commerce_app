12fbf8dfbc8f8ae88eb12eb0be36ed7b50f30dbf
Your Name Amiya <my9090202865@gmail.com>
2024-04-07 14:53:47 +0530

    drop down styling corrected

--- src/components/categorySearchCompo.jsx
+++ src/components/categorySearchCompo.jsx
@@ -21,12 +21,16 @@ const SategorySearchCompo = () => {
     return (
         <>
             <form class="max-w-lg mx-auto">
-                <div class="flex">
-                    <button id="dropdown-button" data-dropdown-toggle="dropdown" onClick={showDropdown} class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{currentCategory == 'all'?"All Category":currentCategory} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <div class="flex relative">
+                    <button id="dropdown-button" data-dropdown-toggle="dropdown" onClick={showDropdown} class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{currentCategory == 'all' ? "All Categories" : currentCategory} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
                     </svg></button>
-                    <div id="dropdown" className={`z-10 ${visible ? "" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+                    <div id="dropdown" className={`z-10 ${visible ? "" : "hidden"} absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+
                         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                            <li>
+                                <button type="button" onClick={() => setCurrentCategory('all')} class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All categories</button>
+                            </li>
                             {categories.map((item) => (<li>
                                 <button type="button" onClick={() => setCurrentCategory(item)} class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item}</button>
                             </li>))}
