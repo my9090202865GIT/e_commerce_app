3f970903f1d5865a5b9a6b00097cff6660f9fbff
Your Name Amiya <my9090202865@gmail.com>
2024-04-07 20:13:03 +0530

    converted  4 cards per row in home component and toast message if loggedin

--- src/home.js
+++ src/home.js
@@ -11,9 +11,6 @@ import toast, { Toaster } from 'react-hot-toast';
 import RatingStar from './components/RatingStar'
 const StyleComp = styled.div`
   
-  
-  
-  
   .container {
     display: flex;
     align-items: center;
@@ -83,6 +80,7 @@ const Home = () => {
   const dispatch = useDispatch()
   const allProducts = useSelector((state) => state.productReducer.allProducts);
   const currentCategory = useSelector((state) => state.productReducer.currentCategory);
+  const logggedIn = useSelector((state) => state.authReducer.isLoggedIn);
   const [showProducts, setshowProducts] = useState()
   const { requireAuth } = useAuth();
 
@@ -120,56 +118,58 @@ const Home = () => {
 
   const addCart = (item) => {
     requireAuth(() => dispatch(addToCart(item)));
-    toast.success("your order has been confirmed");
+    if (logggedIn) {
+      toast.success("your order has been confirmed");
+    }
 
   };
 
 
   return (
     // <StyleComp>
-      <div className='container flex flex-wrap'>
-        {showProducts && showProducts.map((item, index) => {
-          return (
-            <div key={item.id} class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className='container flex flex-wrap mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+      {showProducts && showProducts.map((item, index) => {
+        return (
+          <div key={item.id} class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <Link to={{ pathname: `/product/${item.id}` }}>
+              <img class="p-8 rounded-t-lg w-full h-80" src={item.image} alt="product image" />
+            </Link>
+            <div class="px-5 pb-5">
               <Link to={{ pathname: `/product/${item.id}` }}>
-                <img class="p-8 rounded-t-lg w-full h-80" src={item.image} alt="product image" />
+                <h5 class="text-xl truncate font-semibold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
               </Link>
-              <div class="px-5 pb-5">
-                <Link to={{ pathname: `/product/${item.id}` }}>
-                  <h5 class="text-xl truncate font-semibold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-                </Link>
-                <div class="flex items-center mt-2.5 mb-5">
-                  <RatingStar rating={item?.rating.rate}></RatingStar>
-                </div>
-                <div class="flex items-center justify-between">
-                  <span class="text-3xl font-bold text-gray-900 dark:text-white">&#x20B9; {item.price}</span>
-                  <button onClick={() => addCart(item)} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add to cart</button>
-                </div>
+              <div class="flex items-center mt-2.5 mb-5">
+                <RatingStar rating={item?.rating.rate}></RatingStar>
+              </div>
+              <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-gray-900 dark:text-white">&#x20B9; {item.price}</span>
+                <button onClick={() => addCart(item)} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add to cart</button>
               </div>
             </div>
-            // <div key={item.id} className="card aspect-w-3 aspect-h-4">
-            //   <Link to={{ pathname: `/product/${item.id}` }}>
-            //     <img src={item.image} className="w-full h-48 " alt={item.name} style={{ width: "100%" }} />
-            //   </Link>
-            //   <Link to={{ pathname: `/product/${item.id}` }}>
-            //   <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-            //   </Link>
-            //   <p className="price">&#x20B9; {item.price}</p>
-            //   {/* <p className='desc'>{item.description}</p> */}
-            //   <button
-            //     type="button"
-            //     className="flex items-center space-x-2 hover:bg-blue-500 text-white py-2 px-4 rounded bg-pink-500"
-            //     onClick={() => addCart(item)}
-            //     data-test="add-cart-btn"
-            //   >
-            //     <span>ADD TO CART</span>
-            //   </button>
-            // </div>
-          )
-        })}
+          </div>
+          // <div key={item.id} className="card aspect-w-3 aspect-h-4">
+          //   <Link to={{ pathname: `/product/${item.id}` }}>
+          //     <img src={item.image} className="w-full h-48 " alt={item.name} style={{ width: "100%" }} />
+          //   </Link>
+          //   <Link to={{ pathname: `/product/${item.id}` }}>
+          //   <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
+          //   </Link>
+          //   <p className="price">&#x20B9; {item.price}</p>
+          //   {/* <p className='desc'>{item.description}</p> */}
+          //   <button
+          //     type="button"
+          //     className="flex items-center space-x-2 hover:bg-blue-500 text-white py-2 px-4 rounded bg-pink-500"
+          //     onClick={() => addCart(item)}
+          //     data-test="add-cart-btn"
+          //   >
+          //     <span>ADD TO CART</span>
+          //   </button>
+          // </div>
+        )
+      })}
 
-        <Toaster />
-      </div>
+      <Toaster />
+    </div>
     // </StyleComp>
   )
 }
